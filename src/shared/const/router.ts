f9c1b0888c18f58dff73ec088a535433a3c175cb
asianParadissseee <asian.paradissseee@gmail.com>
2024-03-12 20:40:51 +0600

    add router const

---
+++ src/shared/const/router.ts
@@ -0,0 +1,12 @@
+export enum AppRoutes {
+  HOME = 'home',
+  PROFILE = 'profile',
+  ABOUT_COMPANY = 'about',
+  ARTICLES = 'articles',
+  CATALOG = 'catalog',
+  CARD_BASKET = 'card-basket',
+  CATALOG_PRODUCTS = 'catalog-products',
+  CONTACTS = 'contacts',
+  NEWS = 'news',
+  NOT_FOUND='not-found',
+}
