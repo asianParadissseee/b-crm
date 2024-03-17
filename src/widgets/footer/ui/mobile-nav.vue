63f08cde5776a33c66d52a9e5d27502200bd8d12
asianParadissseee <asian.paradissseee@gmail.com>
2024-03-18 00:29:02 +0600

    add sticky navigation for mobile

--- src/widgets/footer/ui/mobile-nav.vue
+++ src/widgets/footer/ui/mobile-nav.vue
@@ -42,7 +42,7 @@ const navigation = [
 </script>
 
 <template>
-  <footer class="absolute bottom-0 right-0 left-0 z-50 bg-white">
+  <footer class="sticky bottom-0 right-0 left-0 z-50 bg-white">
     <hr class="mb-10 shadow-2xl" />
     <nav class="flex justify-between px-5 items-center container w-full mx-auto">
       <div
