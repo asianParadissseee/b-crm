dea965b9173c93b94cce51bbfb13c21847b45b81
asianParadissseee <asian.paradissseee@gmail.com>
2024-03-28 13:25:52 +0600

    change state to store for auth

--- src/features/auth/models/store/auth.store.ts
+++ src/features/auth/models/store/auth.store.ts
@@ -1,22 +1,41 @@
 import { defineStore } from 'pinia'
-import { AuthDto, AuthResponse } from '../schema/auth.schema.ts'
+import { AuthDto, AuthResponse, AuthSchema } from '../schema/auth.schema.ts'
 import { reactive } from 'vue'
 import { http } from '@/shared/api/axios.ts'
 
 export const useAuthStore = defineStore('auth', () => {
-  const authData: AuthDto = reactive({
-    email: '',
-    fullName: '',
-    password: '',
-    phoneNumber: ''
+  const authState: AuthSchema = reactive({
+    dto: {
+      fullName: '',
+      password: '',
+      email: '',
+      phoneNumber: ''
+    },
+    response: {
+      token: ''
+    },
+    error: '',
+    isError: false,
+    isLoading: false
   })
 
-  async function getAuthDto(values: AuthDto): Promise<AuthResponse> {
-    return (await http.post('/auth', values)).data
+  async function getAuthDto(dto: AuthDto): Promise<AuthResponse | void> {
+    authState.isLoading = true
+    try {
+      authState.isLoading = false
+      const res = await http.post('/auth', dto)
+      if (!res.data) {
+        throw new Error('Ошибка получения данных с авторизации')
+      }
+      authState.response = res.data
+    } catch (e) {
+      authState.isError = true
+      authState.error = '' + e || 'Ошибка авторизации'
+    }
   }
 
   return {
-    authData,
+    authState,
     getAuthDto
   }
 })
